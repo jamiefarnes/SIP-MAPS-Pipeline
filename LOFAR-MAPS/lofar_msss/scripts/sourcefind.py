bf6e752bf5c0ae08f894a8ff71565c869b019c0d
Jamie Farnes <jamie.farnes@oerc.ox.ac.uk>
2019-02-28 12:06:17 +0000

    PyBDSF update

--- LOFAR-MAPS/lofar_msss/scripts/sourcefind.py
+++ LOFAR-MAPS/lofar_msss/scripts/sourcefind.py
@@ -4,11 +4,18 @@
 """
 import sys
 """
-sys.path = ['/home/jovyan/sdp/algorithm-reference-library/']
-"""
 sys.path = ['/usr/lib/python2.7/dist-packages/']
+"""
+sys.path.insert(0,'/usr/lib/python2.7/dist-packages/')
 
 print sys.path
 
 import bdsf
 
+filename = '/data/outputs/MOMENTS/Std-P.fits'
+img = bdsf.process_image(filename, rms_box=(100,100), output_all = True)
+
+img.show_fit()
+img.write_catalog(format='fits', catalog_type='srl')
+
+img.export_image(img_type='gaus_model', outfile=filename+'.model')
