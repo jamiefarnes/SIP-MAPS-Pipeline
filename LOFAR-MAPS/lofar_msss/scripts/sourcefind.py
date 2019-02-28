9ab905ad4dbbe342ae1f65d67b78046c37c6e548
Jamie Farnes <jamie.farnes@oerc.ox.ac.uk>
2019-02-28 15:18:51 +0000

    code cleanup

--- LOFAR-MAPS/lofar_msss/scripts/sourcefind.py
+++ LOFAR-MAPS/lofar_msss/scripts/sourcefind.py
@@ -1,23 +1,25 @@
-"""
-   Hacky source-finding bolt-on, work in progress...
-"""
-"""
-    Change the Python path for Python2:
-"""
+#!/usr/bin/env python
+
+"""sourcefind.py: Run source-finding on the moment images."""
+
 import sys
-"""
-sys.path = ['/usr/lib/python2.7/dist-packages/']
-"""
-sys.path.insert(0,'/usr/lib/python2.7/dist-packages/')
-"""
-print sys.path
-"""
+
 import bdsf
 
-filename = '/data/outputs/MOMENTS/Std-P.fits'
+__author__ = "Jamie Farnes"
+__email__ = "jamie.farnes@oerc.ox.ac.uk"
+
+    
+# Run Source-Finding
+# ------------------------------------------------------
 """
-filename = '/data/outputs/imaging_clean_WStack-1.fits'
+Currently a hacky source-finding bolt-on and work in progress...
 """
+# Change the Python path for Python2:
+sys.path.insert(0,'/usr/lib/python2.7/dist-packages/')
+
+filename = '/data/outputs/MOMENTS/Std-P.fits' #  '/data/outputs/imaging_clean_WStack-1.fits'
+
 img = bdsf.process_image(filename, rms_box=(100,100), output_all = True, frequency= 1.4e9, beam=(60.0, 60.0, 0.0))
 
 print("HERE")
@@ -26,6 +28,3 @@ img.show_fit()
 img.write_catalog(format='fits', catalog_type='srl')
 
 img.export_image(img_type='gaus_model', outfile=filename+'.model')
-
-
-
